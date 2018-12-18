705630a9cfc9c204778cccb86124af1664d0d7b7
JiamingXie <jiaming.xie7@gmail.com>
2018-12-18 13:34:39 +0800

    host driver path setup

--- xilinx-fpga/driver/auto.sh
+++ xilinx-fpga/driver/auto.sh
@@ -13,4 +13,8 @@ docker build -t $img .
 # docker run --privileged --cap-add=ALL -v /etc/OpenCL/vendors:/etc/OpenCL/vendors -v /lib/firmware/xilinx:/lib/firmware/xilinx -v /lib/xilinx/:/lib/xilinx/  $img
 docker run --privileged --cap-add=ALL -v ${OPENCL_VENDOR}:${OPENCL_VENDOR} -v ${XILINX_FIRMWARE}:${XILINX_FIRMWARE} -v ${XILINX_OPENCL}:${XILINX_OPENCL}  $img
 
+setup="setup.sh"
+echo 'export XILINX_OPENCL="/lib/xilinx"' > $setup
+echo 'export LD_LIBRARY_PATH=$XILINX_OPENCL/runtime/lib/x86_64:$LD_LIBRARY_PATH' >> $setup
+echo 'export XCL_PLATFORM=xilinx_adm-pcie-7v3_1ddr_3_0' >> $setup
 # docker run -it --device /dev/xcldma/xcldma0_user:/dev/xcldma/xcldma0_user -v /home/jiaming/FPGA-OpenCL/:/FPGA-OpenCL/ -v /dev/xcldma/:/dev/xcldma/  xilinx_fpga:v5 /bin/bash
